b08195e3142f9b52f5725a1e499a09345167edb7
amshelhack3r <amshelhack3r@gmail.com>
2018-11-26 15:01:58 +0300

    add template directory

--- caterpillar/settings.py
+++ caterpillar/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATE = os.path.join(BASE_DIR, 'hello_caterpillar/templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
